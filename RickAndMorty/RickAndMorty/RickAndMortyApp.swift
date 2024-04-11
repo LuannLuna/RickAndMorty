aceeca5d8ff1fa50d1c68fe4d29bf705c7fccc95
Luann Luna <luann.luna@thoughtworks.com>
2024-04-10 23:36:08 -0300

    created initial project

---
+++ RickAndMorty/RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Luann Marques Luna on 10/04/24.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
